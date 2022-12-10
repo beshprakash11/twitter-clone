977e4331acdc0897314609ec5d77a06826857359
beshprakash <besh@arrtsm.com>
2022-12-10 06:34:57 +0100

    Tweet model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/twitter-api-course',{
     userNewUrlParser: true,
     useCreateIndex: true,
-    useFindandModify: false,
-    useUnifiedIndex: true
+    useFindAndModify: false,
+    useUnifiedTopology: true
 
 });
+//mongoose.set('strictQuery', true);
